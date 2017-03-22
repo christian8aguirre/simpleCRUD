5cdb90438769556c1dd294537ce5a1589884c687
christian8aguirre <christian8.aguirre@icloud.com>
2017-03-22 16:45:12 -0600

    Update main.go

--- main.go
+++ main.go
@@ -10,7 +10,7 @@ import (
 var db *sql.DB
 
 func init() {
-	tmpDB, err := sql.Open("postgres", "user=runhknsftmpuqt dbname=da1qg9a3lcbj6g host=ec2-54-235-240-92.compute-1.amazonaws.com password=89634650246cdb1fbac7e8cbaa00973a708eceda063bb8bc1d108f209c716a44")
+	tmpDB, err := sql.Open("postgres", "url")
 	if err != nil {
 		log.Fatal(err)
 	}
